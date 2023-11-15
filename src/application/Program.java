b441e2de28b3ad080cc63001056dcc9ee0580735
Raphael da Costa Peret <rcperet@gmail.com>
2023-11-15 19:26:41 -0300

    Hendling captured pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -15,12 +17,13 @@ public class Program {
 		
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 		
 		while(true) {
 			try{
 				UI.clearScreen();
 				
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch, captured);
 				
 				System.out.println();
 				System.out.print("Source: ");
@@ -34,6 +37,9 @@ public class Program {
 				ChessPosition taget = UI.readChessPosition(sc);
 				
 				ChessPiece capturePiece = chessMatch.performChessMove(source, taget);
+				if(capturePiece != null) {
+					captured.add(capturePiece);
+				}
 				
 			} catch(ChessException e) {
 				System.out.println(e.getMessage());
